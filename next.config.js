1aecc2becf249ea12f3039340bc650d50c78e7b8
William Fabiano de Almeida <91387155+Cbolla@users.noreply.github.com>
2022-09-02 14:55:43 -0300

    Feat: Modified icons

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+module.exports = {
+  nextConfig,
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    sw:"/sw.js"
+  },
+  reactStrictMode: true,
+}
