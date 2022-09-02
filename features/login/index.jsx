beb03b1dc57c43e9b88a305f8a64d1880d4be87a
William Fabiano de Almeida <91387155+Cbolla@users.noreply.github.com>
2022-09-02 14:23:33 -0300

    Feat: Modified icons

--- features/login/index.jsx
+++ features/login/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Page} from "../../app/components/layout/Page"
 import Logo from "../../public/assets/img/logo.svg"
+import Google from "../../public/assets/icons/google-icon.svg"
 export const Login = ()=> {
   return (
     <div className=''>
@@ -11,6 +12,11 @@ export const Login = ()=> {
             <input className='outline-none outline-[#DEAB14] w-[90%] h-12 border-none rounded-[20px] bg-[#26292F] pl-10' type="text" name="" id="" placeholder='Usuário ou E-mail'/>
             <input className='outline-none outline-[#DEAB14] mt-4 w-[90%] h-12 border-none rounded-[20px] bg-[#26292F] pl-10' type="password" name="" id="" placeholder='Senha'/>
           </div>
+          <p className="mt-4 text-[#9A9A9A] cursor-pointer">Cadastrar-se</p>
+          <button className="bg-[#D5A518] w-44 h-10 rounded-[20px] mt-6">
+            Logar
+          </button>
+          <img className='mt-8' src={Google.src} alt="Conta Google" />
         </div>
       </Page> 
     </div>
