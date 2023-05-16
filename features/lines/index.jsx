db4f0a444fa20a5eb0ebbcaefaaa1a5975628f90
Cbolla <futuro.noob@gmail.com>
2023-05-16 18:52:21 -0300

    Troquei de tabela do google para nova api

--- features/lines/index.jsx
+++ features/lines/index.jsx
@@ -1,75 +1,78 @@
-const sheetdb = require("sheetdb-node");
-import { React, useMemo, useState, useEffect } from 'react'
+import { useMemo, useState, useEffect } from 'react';
 import { Page } from './../../app/components/layout/Page';
 import { Screen } from './../../app/components/layout/Screen';
 import { Header } from './../../app/components/header';
 import { CardSecundary } from './../../app/components/Cards/CardSecundary';
-import Search from "../../public/assets/icons/search-icon.svg"
+import Search from "../../public/assets/icons/search-icon.svg";
 
-const client = sheetdb({ address: 'ko22hgud347gh',auth_login: '1wu6dwvj',
-auth_password: 'smzidxxk452t2t60cqiz'});
+export const Lines = () => {
+  const [linhas, setLinhas] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  export const Lines = () => {
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
 
-    const [linhas,setLinhas] = useState([])
-    const [searchValue, setSearchValue] = useState("");
-    
-    const handleSearch = (e) => {
-      setSearchValue(e.target.value)
-    }
+  const filterSearch = useMemo(() => {
+    !searchValue && linhas;
+    const term = searchValue.toLowerCase();
 
-    const filterSearch = useMemo(() => {
-      !searchValue && linhas
-      const term = searchValue.toLowerCase();
-    
-      const contains = (str) =>
-        str.toLowerCase().indexOf(term) !== -1;
+    const contains = (str) => str.toLowerCase().indexOf(term) !== -1;
 
-      return linhas?.filter((p) => {
-        return contains(p.local) || contains(p.destino);})
-    
-      }, [searchValue,linhas])
+    return linhas?.filter((p) => {
+      return contains(p.local) || contains(p.destino);
+    });
+  }, [searchValue, linhas]);
 
-      //coletar dados
-      useEffect(()=>{
-        const coletar = async () => {
-          let dataReturn
-          await client.read()
-            .then((data)=>{
-            dataReturn = data
-          })
-          return dataReturn
-        
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://sheetsu.com/apis/v1.0su/ad0e6353e76d');
+        const data = await response.json();
+
+        if (Array.isArray(data)) {
+          setLinhas(data);
+          
+        } else {
+          console.error('Invalid data format. Expected an array.');
         }
-        coletar().then((data)=>{
-          setLinhas(JSON.parse(data))
-        }
-      )
-      },[])
-    return (
-      <div>
-        <Page>
-          <Screen>
-            <Header menu="" logo="true" />
-            <h1 className="text-white mt-6 text-xl">LINHAS, ROTAS E HORÁRIOS</h1>
-            <div className='w-full flex flex-col items-center justify-center'>
-              <input onChange={handleSearch} value={searchValue} className='relative text-white focus:outline-none mt-4 pl-4 w-[80%] h-[50px] rounded-[25px] bg-[#26292F]' type="text" name="search" id="search" placeholder="Pesquique Aqui..." />
+      } catch (error) {
+        console.error(error);
+      }
+      console.log( linhas , "olaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    };
+    
+    fetchData();
+  }, []);
 
-              <img className='absolute ml-[60%] mt-3' src={Search.src} alt="Lupa" />
-            </div>
-            <p className="text-white mt-4 mb-2">======Selecione a linha desejada======</p>
-            <div className='ml-3 w-full flex flex-col items-center h-full overflow-y-scroll'>
-              {
-                filterSearch?.map((p,indice) => {
-                  console.log(p)
-                  return <CardSecundary indice={indice} line={p.linha} localization={p.local}destiny={p.destino} mt={"2"} />
-                  
-                })
-              }
+  return (
+    <div>
+      <Page>
+        <Screen>
+          <Header menu="" logo="true" />
+          <h1 className="text-white mt-6 text-xl">LINHAS, ROTAS E HORÁRIOS</h1>
+          <div className="w-full flex flex-col items-center justify-center">
+            <input
+              onChange={handleSearch}
+              value={searchValue}
+              className="relative text-white focus:outline-none mt-4 pl-4 w-[80%] h-[50px] rounded-[25px] bg-[#26292F]"
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Pesquique Aqui..."
+            />
 
-            </div>
-          </Screen>
-        </Page>
-      </div>
-    )
-  }
+            <img className="absolute ml-[60%] mt-3" src={Search.src} alt="Lupa" />
+          </div>
+          <p className="text-white mt-4 mb-2">======Selecione a linha desejada======</p>
+          <div className="ml-3 w-full flex flex-col items-center h-full overflow-y-scroll">
+            {filterSearch?.map((p, indice) => {
+              console.log(p);
+              return <CardSecundary indice={indice} line={p.linha} localization={p.local} destiny={p.destino} mt={"2"} />;
+            })}
+          </div>
+        </Screen>
+      </Page>
+    </div>
+  );
+};
--- pages/hours/[indice].jsx
+++ pages/hours/[indice].jsx
@@ -1,7 +1,39 @@
+import { useRouter } from 'next/router';
+import { useState,useEffect } from 'react';
 import Head from "next/head"
 import {Hours} from "../../features/hours"
 
+
 export default function HoursPage() {
+  const router = useRouter();
+  const { indice } = router.query;
+  // puxando dados do google sheets
+  const [linhas,setLinhas] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://sheetsu.com/apis/v1.0su/ad0e6353e76d');
+        const data = await response.json();
+
+        if (Array.isArray(data)) {
+          setLinhas(data);
+          
+        } else {
+          console.error('Invalid data format. Expected an array.');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      console.log( linhas , "olaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    };
+    
+    fetchData();
+  }, []);
+
+
+
+
   return (
     <div >
           <Head>
@@ -9,7 +41,8 @@ export default function HoursPage() {
             <meta name="keywords" content="onibus,trasporte,publico"/>
             <meta name="description" content="App para ajudar com o tranporte público"/>
         </Head>
-        <Hours/>
+        <Hours tittle="Horarios" partida={linhas[indice]?.local}
+        saida={linhas[indice]?.destino} />
     </div>
   )
 }
